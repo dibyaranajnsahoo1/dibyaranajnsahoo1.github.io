85dbd230040ab551c470fe58de786c6166fee773
dibyaranjan1 <dibyaranjansahoo955@gmail.com>
2023-01-27 20:58:58 +0530

    added

--- js/app.js
+++ js/app.js
@@ -1,31 +1,29 @@
-$(document).ready(function(){
-    $('.slider').slick({
-        arrows:false,
-        dots:true,
-        appendDots:'.slider-dots',
-        dotsClass:'dots'
-    });
-    
-    
-    let hamberger = document.querySelector('.hamberger');
-    let times = document.querySelector('.times');
-    let mobileNav = document.querySelector('.mobile-nav');
-    
-    hamberger.addEventListener('click', function(){
-      mobileNav.classList.add('open');  
-    });
-    
-    times.addEventListener('click', function(){
-        mobileNav.classList.remove('open');  
-    });
-    
-    });
+$(document).ready(function () {
+  $(".slider").slick({
+    arrows: false,
+    dots: true,
+    appendDots: ".slider-dots",
+    dotsClass: "dots",
+  });
 
-    // var typingEffect = new Typed(".MyName",{
-    //     String : ["Coder"],
-    //     loop : false,
-    //     typeSpeed : 100,
-    //     backSpeed :80,
-    //     bacbDelay : 1500
-        
-    // })
+  let hamberger = document.querySelector(".hamberger");
+  let times = document.querySelector(".times");
+  let mobileNav = document.querySelector(".mobile-nav");
+
+  hamberger.addEventListener("click", function () {
+    mobileNav.classList.add("open");
+  });
+
+  times.addEventListener("click", function () {
+    mobileNav.classList.remove("open");
+  });
+});
+
+// var typingEffect = new Typed(".MyName",{
+//     String : ["Coder"],
+//     loop : false,
+//     typeSpeed : 100,
+//     backSpeed :80,
+//     bacbDelay : 1500
+
+// })
